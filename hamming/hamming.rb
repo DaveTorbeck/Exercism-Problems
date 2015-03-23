0abab5f6b3c074ee26eec4d5e1798d282a9619a8
David Torbeck <davetorbeck@gmail.com>
2015-03-23 16:13:55 -0700

    Refactor some answers

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,13 +1,5 @@
 class Hamming
-
   def self.compute(strand_1, strand_2)
-  	raise ArgumentError if strand_1.count != strand_2.count
-
-  	distance = 0
-    strand_1.each_char.with_index do |letter, index|
-      distance += 1 if letter != strand_2[index]
-    end
-    distance 
+    strand_1.each_char.with_index.count {|letter, index| letter != strand_2[index]}
   end
-
 end
--- rna-transcription/complement.rb
+++ rna-transcription/complement.rb
@@ -1,16 +1,10 @@
 class Complement
-  def self.transcript 
-    {"G" => "C",
-     "C" => "G",
-     "T" => "A",
-     "A" => "U"}
-  end
 
   def self.of_dna(d_strand)
     r_strand = ""
     d_strand.each_char.with_index do |letter, index| 
-      raise ArgumentError if self.transcript[letter].nil?
-      r_strand[index] = self.transcript[letter]
+      raise ArgumentError if transcript[letter].nil?
+      r_strand << transcript[letter]
     end
     r_strand
   end
@@ -19,10 +13,19 @@ class Complement
   def self.of_rna(r_strand)
     d_strand = ""
   	r_strand.each_char.with_index do |letter, index| 
-      raise ArgumentError if self.transcript.key(letter).nil?
-      d_strand[index] = self.transcript.key(letter)
+      raise ArgumentError if transcript.key(letter).nil?
+      d_strand << transcript.key(letter)
     end
     d_strand
+  end
+
+  private
+
+  def self.transcript 
+    {"G" => "C",
+     "C" => "G",
+     "T" => "A",
+     "A" => "U"}
   end
 
 end
